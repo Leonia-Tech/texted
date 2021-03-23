257196610f55324684a2684fbe095f98b3fe9d0f
Giovanni Zaccaria <71092108+LoZack19@users.noreply.github.com>
2021-03-23 19:47:48 +0100

    Move color definitions

--- include/texted/edit.h
+++ include/texted/edit.h
@@ -1,9 +1,6 @@
 #ifndef TEXTED_EDIT_H
 #define TEXTED_EDIT_H
 
-#define RED     "\033[31m"
-#define RESET   "\033[0m"
-
 // Trova quante volte il caratter ch compare in str
 int strocc(char* str, char ch);
 
