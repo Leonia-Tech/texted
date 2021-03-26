9aa210881c49b552c9633ecb2409c17208560658
LoZack19 <giozaccaria02@gmail.com>
2021-03-26 16:04:51 +0100

    finfo_s structure added

--- include/texted/permissions.h
+++ include/texted/permissions.h
@@ -3,6 +3,14 @@
 
 #include <sys/stat.h>
 
+typedef struct {
+    char* fi_name;
+    char* fi_permissions;
+    char* fi_user;
+    char* fi_group;
+    char* fi_extension;
+}finfo_s;
+
 // Retrieve octal file permissions
 mode_t get_file_permissions(const char* Filename);
 
@@ -14,6 +22,10 @@ char* get_file_user(const char* Filename);
 
 // Get the group of the file
 char* get_file_group(const char* Filename);
+
+finfo_s* finfo(const char* Filename);
+
+void finfo_free(finfo_s* fi);
 
 // Print file info
 void ed_print_permissions(const char* Filename);
--- src/permissions.c
+++ src/permissions.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <stdint.h>
 
 #include <sys/types.h>
 #include <pwd.h>
@@ -58,24 +59,88 @@ char* get_file_group(const char* Filename)
 	return gr->gr_name;
 }
 
+char get_file_type(const mode_t mode)
+{
+	char* mask = "bcdp-ls";
+	char type = '\0';
+	uint8_t bit_mask = 0;
+
+	bit_mask |= S_ISBLK(mode)  << 6
+			 |  S_ISCHR(mode)  << 5
+			 |  S_ISDIR(mode)  << 4
+			 |  S_ISFIFO(mode) << 3
+			 |  S_ISREG(mode)  << 2
+			 |  S_ISLNK(mode)  << 1
+			 |  S_ISSOCK(mode) << 0;
+	
+	for(uint8_t b = 1, i = 0; i < 7; b <<= 1, i++) {
+		if(b & bit_mask) {
+			type = mask[i];
+			break;
+		}
+	}
+	
+	return type;
+}
+
 char* get_string_permissions(const mode_t mode)
 {
 	const size_t size = 8;
-	char* permissions = strdup("---------");
+	char* permissions = strdup("----------");
 	char* mask = "rwxrwxrwx";
 
+	permissions[0] = get_file_type(mode);
 	for(size_t i = 0; i < 9; i++)
 		if(mode & (1 << i))
-			permissions[size - i] = mask[size - i];
+			permissions[size + 1 - i] = mask[size - i];
 	
 	return permissions;
 }
 
+finfo_s* finfo(const char* Filename)
+{
+	char* tmp;
+	finfo_s* fi = malloc(sizeof(finfo_s));
+	char* fname = strdup(Filename);
+
+	fi->fi_name = strdup(Filename);
+	fi->fi_permissions = get_string_permissions(get_file_permissions(Filename));
+	
+	if(strchr(Filename, '.')) {
+		tmp = strtok(fname, ".");
+		tmp = strtok(NULL, ".");
+		fi->fi_extension = strdup(tmp);
+	} else {
+		fi->fi_extension = NULL;
+	}
+
+	//! Check if free can be done
+	fi->fi_user = get_file_user(Filename);
+	fi->fi_group = get_file_group(Filename);
+
+	return fi;
+}
+
+void finfo_free(finfo_s* fi)
+{
+	free(fi->fi_name);
+	free(fi->fi_permissions);
+	//free(fi->fi_user);
+	//free(fi->fi_group);
+	if(fi->fi_extension)
+		free(fi->fi_extension);
+
+	free(fi);
+}
+
 void ed_print_permissions(const char* Filename)
 {
-	printf(BOLD MAGENTA "-%s\t%s %s\t%s\n" RESET,
-		   get_string_permissions(get_file_permissions(Filename)),
-		   get_file_user(Filename),
-		   get_file_group(Filename),
-		   Filename);
+	finfo_s* fi = finfo(Filename);
+	printf(BOLD MAGENTA "%s\t%s %s\t%s\n" RESET,
+		   fi->fi_permissions,
+		   fi->fi_user,
+		   fi->fi_group,
+		   fi->fi_name);
+	
+	finfo_free(fi);
 }
